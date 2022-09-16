912fbd938275057b4bd81c36b33e71198d6c1945
PSD Wizard <punchey@fullstack.ph>
2022-09-16 14:07:11 +0800

    Final Output

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,10 +1,11 @@
-import { Fragment } from 'react';
-import { useRouter } from 'next/router';
-import { getEventById } from '../../dummy-data';
-import EventSummary from '../../components/event-detail/event-summary';
-import EventLogistics from '../../components/event-detail/event-logistics';
-import EventContent from '../../components/event-detail/event-content';
-
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert";
 
 const EventDetailPage = () => {
   const router = useRouter();
@@ -14,22 +15,31 @@ const EventDetailPage = () => {
 
   //If no item(Falsy)
   if (!event) {
-    return <p>No event found!</p>;
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No event found!</p>;
+        </ErrorAlert>
+        <div className="center">
+          <Button>Show All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
   return (
     <Fragment>
       <EventSummary title={event.title} />
-      <EventLogistics 
-        date={event.date} 
-        address={event.location} 
+      <EventLogistics
+        date={event.date}
+        address={event.location}
         image={event.image}
         alt={event.title}
-       />
+      />
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
     </Fragment>
-  )
-}
+  );
+};
 export default EventDetailPage;
